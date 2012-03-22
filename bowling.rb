2f4a9ed9b4781c2d43f934f8b63b25ef7bce7b34
Sam Starling <sam.starling@gmail.com>
2012-03-22 12:25:54 +0000

    Adding Frame class

--- bowling.rb
+++ bowling.rb
@@ -1,10 +1,27 @@
-class BowlingGame
+class Game
   
-  def add_roll x, y
+  def add_frame frame
+    #
   end
   
   def get_score
     0
+  end
+  
+end
+
+class Frame
+  
+  def initialize a, b
+    @a = a
+    @b = b
+    if get_score > 10 or get_score < 0
+      raise "Invalid score"
+    end
+  end
+  
+  def get_score
+    @a + @b
   end
   
 end
--- test.rb
+++ test.rb
@@ -1,15 +1,56 @@
 require 'test/unit'
 require './bowling'
 
-class BowlingTest < Test::Unit::TestCase
+class FrameTest < Test::Unit::TestCase
+
+  def test_a_single_gutter_ball
+    frame = Frame.new 0, 0
+    assert_equal 0, frame.get_score 
+  end
+  
+  def test_a_first_half_score_is_counted
+    frame = Frame.new 3, 0
+    assert_equal 3, frame.get_score 
+  end
+  
+  def test_a_second_half_score_is_counted
+    frame = Frame.new 0, 5
+    assert_equal 5, frame.get_score 
+  end
+  
+  def test_both_halves_of_frame_are_added
+    frame = Frame.new 2, 1
+    assert_equal 3, frame.get_score
+  end
+  
+  def test_a_score_above_max_is_rejected
+    assert_raise RuntimeError do
+      Frame.new 10, 1
+    end
+  end
+  
+  def test_a_score_below_min_is_rejected
+    assert_raise RuntimeError do
+      Frame.new -1, 0
+    end
+  end
+
+end
+
+class GameTest < Test::Unit::TestCase
 
   def setup
-    @game = BowlingGame.new
+    @game = Game.new
   end
 
   def test_a_single_gutter_ball
-    @game.add_roll 0, 0
+    frame = Frame.new 0, 0
+    @game.add_frame frame
     assert_equal 0, @game.get_score
+  end
+  
+  def test_a_single_point
+    
   end
 
 end
